91f019285a9c7f086c16d3a844f3290865d7ae78
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:19:04 +0500

    Update Light.java

--- src/src/Light.java
+++ src/src/Light.java
@@ -1,1 +1,23 @@
+interface SmartDevice {
+    void turnOn();
+    void turnOff();
+}
+
+public class Light implements SmartDevice {
+    private String name;
+
+    public Light(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println(name + " Light is ON");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println(name + " Light is OFF");
+    }
+}
 
