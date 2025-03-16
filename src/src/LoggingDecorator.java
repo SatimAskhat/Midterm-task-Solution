e9fe92210a22cf638c9344a3719e587365be2367
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:18:31 +0500

    Update LoggingDecorator.java

--- src/src/LoggingDecorator.java
+++ src/src/LoggingDecorator.java
@@ -1,1 +1,18 @@
+public class LoggingDecorator extends Decorator {
+    public LoggingDecorator(SmartDevice device) {
+        super(device);
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Logging: Turning on device");
+        super.turnOn();
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Logging: Turning off device");
+        super.turnOff();
+    }
+}
 
