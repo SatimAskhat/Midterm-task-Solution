9d77615ab8eb95194103d48bb493fd57f4c08255
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:16:48 +0500

    Update SmartHomeFactory.java

--- src/src/SmartHomeFactory.java
+++ src/src/SmartHomeFactory.java
@@ -1,1 +1,11 @@
+interface SmartHomeFactory {
+    Light createLight(String name);
+}
+
+public class BasicSmartHomeFactory implements SmartHomeFactory {
+    @Override
+    public Light createLight(String name) {
+        return new Light(name);
+    }
+}
 
