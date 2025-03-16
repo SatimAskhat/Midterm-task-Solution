7ef5ffecada22b05868600c6470f38a6aa5f74d6
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:35:22 +0500

    Create LegacyLockSystem.java

---
+++ src/src/LegacyLockSystem.java
@@ -0,0 +1,8 @@
+public class LegacyLockSystem {
+    public void activateLock() {
+        System.out.println("Старый замок активирован.");
+    }
+    public void deactivateLock() {
+        System.out.println("Старый замок деактивирован.");
+    }
+}
