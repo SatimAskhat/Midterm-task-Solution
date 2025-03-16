49f2cc5777375b30aee181fef19ff65240979955
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:18:09 +0500

    Update Adapter.java

--- src/src/Adapter.java
+++ src/src/Adapter.java
@@ -1,1 +1,28 @@
+class LegacyLockSystem {
+    public void activateLock() {
+        System.out.println("Legacy Lock System: Locking doors");
+    }
+
+    public void deactivateLock() {
+        System.out.println("Legacy Lock System: Unlocking doors");
+    }
+}
+
+public class Adapter implements SmartDevice {
+    private LegacyLockSystem legacyLock;
+
+    public Adapter(LegacyLockSystem legacyLock) {
+        this.legacyLock = legacyLock;
+    }
+
+    @Override
+    public void turnOn() {
+        legacyLock.activateLock();
+    }
+
+    @Override
+    public void turnOff() {
+        legacyLock.deactivateLock();
+    }
+}
 
