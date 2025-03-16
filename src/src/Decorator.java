aec62b70d9361e1b132c8813fd998d52e28259e2
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:19:22 +0500

    Update Decorator.java

--- src/src/Decorator.java
+++ src/src/Decorator.java
@@ -1,1 +1,18 @@
+public abstract class Decorator implements SmartDevice {
+    protected SmartDevice decoratedDevice;
+
+    public Decorator(SmartDevice device) {
+        this.decoratedDevice = device;
+    }
+
+    @Override
+    public void turnOn() {
+        decoratedDevice.turnOn();
+    }
+
+    @Override
+    public void turnOff() {
+        decoratedDevice.turnOff();
+    }
+}
 
