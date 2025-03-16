b901ffe539c77e3232dae1fbb17e548dd40375ba
SatimAskhat <s23067494@narxoz.kz>
2025-03-16 14:30:03 +0500

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,21 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         SmartHomeFactory factory = new BasicSmartHomeFactory();
-        Light kitchenLight = factory.createLight("Kitchen");
-        Light bedroomLight = factory.createLight("Bedroom");
+        Light kitchenLight = factory.createLight("Кухня");
+        Light bedroomLight = factory.createLight("Спальня");
 
         SmartDevice loggedKitchenLight = new LoggingDecorator(kitchenLight);
-
-        Room home = new Room("Home");
-        home.addDevice(loggedKitchenLight);
-        home.addDevice(bedroomLight);
-
         LegacyLockSystem oldLock = new LegacyLockSystem();
         SmartDevice lockAdapter = new Adapter(oldLock);
-        home.addDevice(lockAdapter);
 
-        home.turnOn();
-        home.turnOff();
+        Room livingRoom = new Room("Гостиная");
+        livingRoom.addDevice(bedroomLight);
+        livingRoom.addDevice(loggedKitchenLight);
+
+        SmartHomeSystem smartHome = new SmartHomeSystem();
+        smartHome.addDevice(livingRoom);
+        smartHome.addDevice(lockAdapter);
+
+        smartHome.showStatus();
+        smartHome.turnAllOn();
+        smartHome.turnAllOff();
     }
 }
-
